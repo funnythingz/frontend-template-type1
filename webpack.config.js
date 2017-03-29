db23c65b13f50c84f9ac88053f214ea81e251f30
funnythingz <funnythingz69+github@gmail.com>
2017-03-29 19:41:12 +0900

    wip webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,23 @@
+var webpack = require('webpack');
 var path = require('path');
 
 module.exports = {
-    entry: './src/app.js',
+    devtool: 'cheap-module-source-map',
+    entry: {
+        'dist/app.js': path.resolve(__dirname, 'src', 'app.js')
+    },
     output: {
         filename: 'app.js',
-        path: path.resolve(__dirname, 'dist')
-    }
+        path: path.resolve(__dirname, 'dist'),
+        publicPath: path.resolve(__dirname, 'assets'),
+        sourceMapFilename: '[name].map'
+    },
+    plugins: [
+        new webpack.optimize.UglifyJsPlugin({
+            compress: {
+                warnings: false,
+                drop_console: false,
+            }
+        }),
+    ]
 };
